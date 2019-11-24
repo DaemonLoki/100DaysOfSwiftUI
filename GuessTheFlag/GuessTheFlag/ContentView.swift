664e9227717dc9c42f395c565feb3dbde969f168
Stefan Blos <stefan.blos@gmail.com>
2019-11-24 08:28:25 +0100

    (Day 21) created GuessTheFlag with required assets

---
+++ GuessTheFlag/GuessTheFlag/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  GuessTheFlag
+//
+//  Created by Stefan Blos on 24.11.19.
+//  Copyright © 2019 Stefan Blos. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
