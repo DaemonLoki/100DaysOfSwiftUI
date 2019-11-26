c49f95e7ef60704ecaef2bbc8d666e54c912c031
Stefan Blos <stefan.blos@gmail.com>
2019-11-27 00:41:29 +0100

    (Day 25) implemented rest of the game

--- The Brain Game/The Brain Game/ContentView.swift
+++ The Brain Game/The Brain Game/ContentView.swift
@@ -15,48 +15,215 @@ struct ContentView: View {
     @State private var playerShouldWin = Bool.random()
     
     @State private var currentScore = 0
+    @State private var currentRound = 1
+    @State private var gameRunning = true
     
     var body: some View {
         VStack {
             Group {
-                Text("Current score: \(currentScore)")
-                    .font(.largeTitle)
-                    .fontWeight(.black)
-                    .padding(.top)
+                TopCard(score: currentScore, round: currentRound, gameRunning: gameRunning)
+                
                 Divider()
-                Text(moves[selectedMove])
-                    .font(.largeTitle)
-                    .fontWeight(.bold)
-                Text(playerShouldWin ? "WIN" : "LOSE")
-                    .font(.title)
-                    .foregroundColor(playerShouldWin ? .green : .red)
-            }
-            
-            Group {
-                HStack {
-                    Button(action: {
-                        print("Rock")
-                    }) {
-                        Text("Rock")
+                    .padding(.bottom)
+                
+                if gameRunning {
+                    ComputerMove(selectedMove: moves[selectedMove], playerShouldWin: playerShouldWin)
+                    
+                    MovesList { (moveIndex) in
+                        self.evaluate(move: moveIndex)
                     }
-                    Spacer()
-                    Button(action: {
-                        print("Paper")
-                    }) {
-                        Text("Paper")
-                    }
-                    Spacer()
-                    Button(action: {
-                        print("Scissors")
-                    }) {
-                        Text("Scissors")
+                } else {
+                    FinalScore(score: currentScore) {
+                        self.restartGame()
                     }
                 }
-            }.padding(.top, 100)
-                .padding(50)
-            
-            Spacer()
+                
+                Spacer()
+            }
         }
+    }
+    
+    func evaluate(move: Int) {
+        let correctMove: Int
+        if selectedMove == 0 {
+            // Rock
+            correctMove = playerShouldWin ? 1 : 2
+        } else if selectedMove == 1 {
+            // Paper
+            correctMove = playerShouldWin ? 2 : 0
+        } else {
+            // Scissors
+            correctMove = playerShouldWin ? 0 : 1
+        }
+        currentScore += move == correctMove ? 1 : -1
+        if currentRound < 10 {
+            createNewRound()
+        } else {
+            gameRunning = false
+        }
+    }
+    
+    func createNewRound() {
+        selectedMove = Int.random(in: 0...2)
+        playerShouldWin = Bool.random()
+        currentRound += 1
+    }
+    
+    func restartGame() {
+        currentRound = 0
+        currentScore = 0
+        createNewRound()
+        gameRunning = true
+    }
+}
+
+struct FinalScore: View {
+    let score: Int
+    let completionHandler: () -> Void
+    
+    var body: some View {
+        Group {
+            VStack {
+                Text("Final score".uppercased())
+                    .font(.title)
+                    .fontWeight(.light)
+                    .foregroundColor(.secondary)
+                
+                
+                Text("\(score)")
+                    .font(.system(size: 80))
+                    .fontWeight(.black)
+                    .padding()
+                
+                Text(score > 5 ? "🎉🎉🎉" : "😥😥😥")
+                    .font(.largeTitle)
+                
+                Text(score > 5 ? "Awesome, keep it going!" : "Better luck next time!")
+                    .font(.headline)
+                    .padding()
+                
+                Button(action: {
+                    self.completionHandler()
+                }) {
+                    VStack {
+                        Image(systemName: "arrow.counterclockwise")
+                            .renderingMode(.original)
+                            .imageScale(.large)
+                            .frame(minWidth: 0, maxWidth: 100, minHeight: 0, maxHeight: 100)
+                        
+                        Text("Restart".uppercased())
+                            .font(.caption)
+                            .fontWeight(.light)
+                            .foregroundColor(.secondary)
+                    }
+                    .padding()
+                    .frame(minWidth: 0, maxWidth: 150, minHeight: 0, maxHeight: 150)
+                    .background(Color.init(.secondarySystemBackground))
+                    .clipShape(RoundedRectangle(cornerRadius: 5))
+                    .padding()
+                    .shadow(radius: 5)
+                }
+            }
+        }
+    }
+}
+
+struct ComputerMove: View {
+    let selectedMove: String
+    let playerShouldWin: Bool
+    
+    var body: some View {
+        Group {
+            Text("Computer chooses".uppercased())
+                .font(.subheadline)
+                .foregroundColor(.secondary)
+            
+            Text(selectedMove.uppercased())
+                .font(.largeTitle)
+                .fontWeight(.black)
+                .padding()
+            
+            Text("You want to".uppercased())
+                .font(.subheadline)
+                .foregroundColor(.secondary)
+            
+            Text(playerShouldWin ? "WIN" : "LOSE")
+                .font(.title)
+                .foregroundColor(playerShouldWin ? .green : .red)
+        }
+    }
+}
+
+struct MoveCard : View {
+    let move: String
+    let name: String
+    
+    var body: some View {
+        VStack(spacing: 20) {
+            Text(move)
+                .font(.largeTitle)
+            Text(name.uppercased())
+                .font(.caption)
+                .fontWeight(.light)
+                .foregroundColor(.secondary)
+        }
+        .padding()
+        .frame(minWidth: 0, maxWidth: .infinity)
+        .background(Color.init(.secondarySystemBackground))
+        .clipShape(RoundedRectangle(cornerRadius: 5))
+        .padding()
+        .shadow(radius: 5)
+    }
+}
+
+struct MovesList: View {
+    let evaluator: (Int) -> Void
+    
+    var body: some View {
+        Group {
+            HStack(spacing: 5) {
+                Button(action: {
+                    self.evaluator(0)
+                }) {
+                    MoveCard(move: "✊🏽", name: "Rock")
+                }
+                Button(action: {
+                    self.evaluator(1)
+                }) {
+                    MoveCard(move: "✋🏽", name: "Paper")
+                }
+                Button(action: {
+                    self.evaluator(2)
+                }) {
+                    MoveCard(move: "✌🏽", name: "Scissors")
+                }
+            }
+        }.padding()
+    }
+}
+
+struct TopCard: View {
+    let score: Int
+    let round: Int
+    let gameRunning: Bool
+    
+    var body: some View {
+        VStack {
+            Text("ROUND \(round) / 10")
+                .font(.caption)
+                .padding()
+            
+            Text("Current score: \(score)")
+                .font(.title)
+                .fontWeight(.bold)
+        }
+        .frame(maxWidth: .infinity)
+        .padding()
+        .background(gameRunning ? Color.green : Color.red)
+        .clipShape(RoundedRectangle(cornerRadius: 10))
+        .padding()
+        .shadow(radius: 10)
+        .blur(radius: gameRunning ? 0 : 5)
     }
 }
 
