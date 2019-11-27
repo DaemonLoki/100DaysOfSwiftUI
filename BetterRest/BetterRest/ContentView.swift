732e05798de61c5f52dd56f252424b0094d6e586
Stefan Blos <stefan.blos@gmail.com>
2019-11-27 09:33:03 +0100

    (Day 26) setup project

---
+++ BetterRest/BetterRest/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  BetterRest
+//
+//  Created by Stefan Blos on 27.11.19.
+//  Copyright © 2019 Stefan Blos. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
