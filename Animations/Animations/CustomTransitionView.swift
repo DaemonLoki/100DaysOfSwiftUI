d94f0ad6440b6954cd539405c20e0f7cad8c2c35
Stefan Blos <stefan.blos@gmail.com>
2019-12-02 15:59:07 +0100

    (Day 33) refactoring

--- Animations/Animations/CustomTransitionView.swift
+++ Animations/Animations/CustomTransitionView.swift
@@ -29,6 +29,24 @@ struct CustomTransitionView: View {
     }
 }
 
+extension AnyTransition {
+    static var pivot: AnyTransition {
+        .modifier(
+            active: CornerRotateModifier(amount: -90, anchor: .topLeading),
+            identity: CornerRotateModifier(amount: 0, anchor: .topLeading)
+        )
+    }
+}
+
+struct CornerRotateModifier: ViewModifier {
+    let amount: Double
+    let anchor: UnitPoint
+
+    func body(content: Content) -> some View {
+        content.rotationEffect(.degrees(amount), anchor: anchor).clipped()
+    }
+}
+
 struct CustomTransitionView_Previews: PreviewProvider {
     static var previews: some View {
         CustomTransitionView()
