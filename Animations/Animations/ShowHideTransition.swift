f189cfaaf958c2e42027caccfa2456d843eadba6
Stefan Blos <stefan.blos@gmail.com>
2019-12-02 15:39:38 +0100

    (Day 33) added showing and hiding transition

---
+++ Animations/Animations/ShowHideTransition.swift
@@ -0,0 +1,36 @@
+//
+//  ShowHideTransition.swift
+//  Animations
+//
+//  Created by Stefan Blos on 02.12.19.
+//  Copyright © 2019 Stefan Blos. All rights reserved.
+//
+
+import SwiftUI
+
+struct ShowHideTransition: View {
+    @State private var isShowingRed = false
+    
+    var body: some View {
+        VStack {
+            Button("Tap me") {
+                withAnimation {
+                    self.isShowingRed.toggle()
+                }
+            }
+            
+            if isShowingRed {
+                Rectangle()
+                    .fill(Color.red)
+                    .frame(width: 200, height: 200)
+                    .transition(.asymmetric(insertion: .scale, removal: .slide))
+            }
+        }
+    }
+}
+
+struct ShowHideTransition_Previews: PreviewProvider {
+    static var previews: some View {
+        ShowHideTransition()
+    }
+}
