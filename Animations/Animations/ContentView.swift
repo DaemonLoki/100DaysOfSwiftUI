c3305a015ecfdfbcbd78f0608f9ecfc51f5b9590
Stefan Blos <stefan.blos@gmail.com>
2019-12-02 13:59:23 +0100

    (Day 32) created implicit animations

---
+++ Animations/Animations/ContentView.swift
@@ -0,0 +1,42 @@
+//
+//  ContentView.swift
+//  Animations
+//
+//  Created by Stefan Blos on 02.12.19.
+//  Copyright © 2019 Stefan Blos. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    
+    @State private var animationAmount: CGFloat = 1
+    
+    var body: some View {
+        Button("Tap Me") {
+            // self.animationAmount += 1
+        }
+        .padding(50)
+        .background(Color.red)
+        .foregroundColor(Color.white)
+        .clipShape(Circle())
+        .overlay(Circle()
+            .stroke(Color.red)
+            .scaleEffect(animationAmount)
+            .opacity(Double(2 - animationAmount))
+            .animation(
+                Animation.easeInOut(duration: 1)
+                    .repeatForever(autoreverses: false)
+            )
+        )
+        .onAppear {
+            self.animationAmount = 2
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
