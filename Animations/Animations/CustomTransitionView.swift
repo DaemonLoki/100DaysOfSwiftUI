7e6ee76bde786bf3232e5bffd1271025b7be8d0b
Stefan Blos <stefan.blos@gmail.com>
2019-12-02 15:49:30 +0100

    (Day 33) custom transitions

---
+++ Animations/Animations/CustomTransitionView.swift
@@ -0,0 +1,36 @@
+//
+//  CustomTransitionView.swift
+//  Animations
+//
+//  Created by Stefan Blos on 02.12.19.
+//  Copyright © 2019 Stefan Blos. All rights reserved.
+//
+
+import SwiftUI
+
+struct CustomTransitionView: View {
+    @State private var isShowingRed = false
+    
+    var body: some View {
+        VStack {
+            Button("Tap me") {
+                withAnimation {
+                    self.isShowingRed.toggle()
+                }
+            }
+            
+            if isShowingRed {
+                Rectangle()
+                    .fill(Color.red)
+                    .frame(width: 200, height: 200)
+                    .transition(.pivot)
+            }
+        }
+    }
+}
+
+struct CustomTransitionView_Previews: PreviewProvider {
+    static var previews: some View {
+        CustomTransitionView()
+    }
+}
