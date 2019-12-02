78e0aa8842fb00fcbb6758e93680c4b8526867a1
Stefan Blos <stefan.blos@gmail.com>
2019-12-02 14:10:10 +0100

    (Day 32) animating bindings

--- Animations/Animations/ContentView.swift
+++ Animations/Animations/ContentView.swift
@@ -13,24 +13,22 @@ struct ContentView: View {
     @State private var animationAmount: CGFloat = 1
     
     var body: some View {
-        Button("Tap Me") {
-            // self.animationAmount += 1
-        }
-        .padding(50)
-        .background(Color.red)
-        .foregroundColor(Color.white)
-        .clipShape(Circle())
-        .overlay(Circle()
-            .stroke(Color.red)
-            .scaleEffect(animationAmount)
-            .opacity(Double(2 - animationAmount))
-            .animation(
+        VStack {
+            Stepper("Scale amount", value: $animationAmount.animation(
                 Animation.easeInOut(duration: 1)
-                    .repeatForever(autoreverses: false)
-            )
-        )
-        .onAppear {
-            self.animationAmount = 2
+                .repeatCount(3, autoreverses: true)), in: 1...10)
+            .padding()
+            
+            Spacer()
+            
+            Button("Tap me") {
+                self.animationAmount += 1
+            }
+            .padding(40)
+            .background(Color.red)
+            .foregroundColor(.white)
+            .clipShape(Circle())
+            .scaleEffect(animationAmount)
         }
     }
 }
