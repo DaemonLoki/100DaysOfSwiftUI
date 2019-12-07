9e2db601b051330dc6a2df51bb3e8f93ee3d5674
Stefan Blos <stefan.blos@gmail.com>
2019-12-07 11:33:09 +0100

    (Day 36) ObservedObject example

---
+++ iExpense/iExpense/ContentView.swift
@@ -0,0 +1,34 @@
+//
+//  ContentView.swift
+//  iExpense
+//
+//  Created by Stefan Blos on 07.12.19.
+//  Copyright © 2019 Stefan Blos. All rights reserved.
+//
+
+import SwiftUI
+
+class User: ObservableObject {
+    @Published var firstName = "Bilbo"
+    @Published var lastName = "Baggins"
+}
+
+struct ContentView: View {
+    @ObservedObject var user = User()
+    
+    var body: some View {
+        VStack(spacing: 30) {
+            Text("Your name is \(user.firstName) \(user.lastName)")
+            
+            TextField("First name", text: $user.firstName)
+            TextField("Last name", text: $user.lastName)
+        }
+    .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
