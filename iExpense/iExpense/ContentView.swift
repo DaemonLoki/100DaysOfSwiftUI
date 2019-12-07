088fb18e641910adf8ebb58c1a655a89939d3a77
Stefan Blos <stefan.blos@gmail.com>
2019-12-07 11:54:20 +0100

    (Day 36) Deleting items from list example

--- iExpense/iExpense/ContentView.swift
+++ iExpense/iExpense/ContentView.swift
@@ -9,31 +9,31 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var showingSheet = false
+    @State private var numbers = [Int]()
+    @State private var currentNumber = 1
     
     var body: some View {
-        Button("Show Sheet") {
-            self.showingSheet.toggle()
-        }
-        .sheet(isPresented: $showingSheet) {
-            SecondView(name: "@stefanjblos")
+        NavigationView {
+            VStack {
+                List {
+                    ForEach(numbers, id: \.self) {
+                        Text("\($0)")
+                    }
+                .onDelete(perform: removeRows)
+                }
+                
+                Button("Add number") {
+                    self.numbers.append(self.currentNumber)
+                    self.currentNumber += 1
+                }
+            }
+            .navigationBarTitle("Deletable Items")
+            .navigationBarItems(leading: EditButton())
         }
     }
-}
-
-struct SecondView: View {
-    var name: String
     
-    @Environment(\.presentationMode) var presentationMode
-    
-    var body: some View {
-        VStack {
-            Text("Hello, \(name)!")
-                
-            Button("Dismiss") {
-                self.presentationMode.wrappedValue.dismiss()
-            }
-        }
+    func removeRows(at offsets: IndexSet) {
+        numbers.remove(atOffsets: offsets)
     }
 }
 
