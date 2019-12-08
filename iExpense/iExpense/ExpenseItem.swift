9327dd422c1949a2c3f857aa8721d1f1b842bf01
Stefan Blos <stefan.blos@gmail.com>
2019-12-08 16:07:55 +0100

    (Day 37) make use of Identifiable

--- iExpense/iExpense/ExpenseItem.swift
+++ iExpense/iExpense/ExpenseItem.swift
@@ -5,8 +5,10 @@
 //  Created by Stefan Blos on 08.12.19.
 //  Copyright © 2019 Stefan Blos. All rights reserved.
 //
+import Foundation
 
-struct ExpenseItem {
+struct ExpenseItem: Identifiable {
+    let id = UUID()
     let name: String
     let type: String
     let amount: Int
