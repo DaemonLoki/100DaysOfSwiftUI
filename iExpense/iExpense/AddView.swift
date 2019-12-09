99ca0f78900fefe1298d9f02917b98036b802dc9
Stefan Blos <stefan.blos@gmail.com>
2019-12-09 13:58:07 +0100

    (Day 37) finished project

--- iExpense/iExpense/AddView.swift
+++ iExpense/iExpense/AddView.swift
@@ -11,6 +11,7 @@ import SwiftUI
 struct AddView: View {
     
     @ObservedObject var expenses: Expenses
+    @Environment(\.presentationMode) var presentationMode
     
     @State private var name = ""
     @State private var amount = ""
@@ -46,6 +47,7 @@ struct AddView: View {
                 if let actualAmount = Int(self.amount) {
                     let item = ExpenseItem(name: self.name, type: self.type, amount: actualAmount)
                     self.expenses.items.append(item)
+                    self.presentationMode.wrappedValue.dismiss()
                 }
             })
         }
