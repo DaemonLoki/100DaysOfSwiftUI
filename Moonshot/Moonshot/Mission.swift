fe984a1e617975aadf64fe054401805b00ec2b4b
Stefan Blos <stefan.blos@gmail.com>
2019-12-11 18:05:47 +0100

    (Day 40) showing list of missions

---
+++ Moonshot/Moonshot/Mission.swift
@@ -0,0 +1,40 @@
+//
+//  Mission.swift
+//  Moonshot
+//
+//  Created by Stefan Blos on 11.12.19.
+//  Copyright © 2019 Stefan Blos. All rights reserved.
+//
+
+import Foundation
+
+struct Mission: Codable, Identifiable {
+    
+    var displayName: String {
+        "Apollo \(id)"
+    }
+    
+    var image: String {
+        "apollo\(id)"
+    }
+    
+    var formattedLaunchDate: String {
+        if let launchDate = launchDate {
+            let formatter = DateFormatter()
+            formatter.dateStyle = .long
+            return formatter.string(from: launchDate)
+        } else {
+            return "N/A"
+        }
+    }
+    
+    struct CrewRole: Codable {
+        let name: String
+        let role: String
+    }
+    
+    let id: Int
+    let launchDate: Date?
+    let crew: [CrewRole]
+    let description: String
+}
