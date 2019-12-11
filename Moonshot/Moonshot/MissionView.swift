5f4f690c48eaddb9be37306a06da42c930968994
Stefan Blos <stefan.blos@gmail.com>
2019-12-11 18:23:39 +0100

    (Day 41) added more info to the MissionView

---
+++ Moonshot/Moonshot/MissionView.swift
@@ -0,0 +1,66 @@
+//
+//  MissionView.swift
+//  Moonshot
+//
+//  Created by Stefan Blos on 11.12.19.
+//  Copyright © 2019 Stefan Blos. All rights reserved.
+//
+
+import SwiftUI
+
+struct MissionView: View {
+    
+    let mission: Mission
+    
+    struct CrewMember {
+        let role: String
+        let astronaut: Astronaut
+    }
+    
+    let astronauts: [CrewMember]
+    
+    init(mission: Mission, astronauts: [Astronaut]) {
+        self.mission = mission
+        
+        var matches = [CrewMember]()
+        
+        for member in mission.crew {
+            if let match = astronauts.first(where: { $0.id ==  member.name }) {
+                matches.append(CrewMember(role: member.role, astronaut: match))
+            } else {
+                fatalError("Missing \(member)")
+            }
+        }
+        
+        self.astronauts = matches
+    }
+    
+    var body: some View {
+        GeometryReader { geometry in
+            ScrollView(.vertical) {
+                VStack {
+                    Image(self.mission.image)
+                        .resizable()
+                        .scaledToFit()
+                        .frame(maxWidth: geometry.size.width * 0.7)
+                        .padding(.top)
+                    
+                    Text(self.mission.description)
+                        .padding()
+                    
+                    Spacer(minLength: 25)
+                }
+            }
+        }
+        .navigationBarTitle(Text(mission.displayName), displayMode: .inline)
+    }
+}
+
+struct MissionView_Previews: PreviewProvider {
+    static let missions: [Mission] = Bundle.main.decode("missions.json")
+    static let astronauts: [Astronaut] = Bundle.main.decode("astronauts.json")
+    
+    static var previews: some View {
+        MissionView(mission: missions[0], astronauts: astronauts)
+    }
+}
