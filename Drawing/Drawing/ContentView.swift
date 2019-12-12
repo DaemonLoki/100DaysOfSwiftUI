dc563d47bf7b29646bd0436c437bca966ea89f66
Stefan Blos <stefan.blos@gmail.com>
2019-12-12 14:03:18 +0100

    (Day 43) transormed Arc into InsettableShape

--- Drawing/Drawing/ContentView.swift
+++ Drawing/Drawing/ContentView.swift
@@ -23,10 +23,12 @@ struct ContentView: View {
         }
     }
     
-    struct Arc: Shape {
+    struct Arc: InsettableShape {
         var startAngle: Angle
         var endAngle: Angle
         var clockwise: Bool
+        
+        var insetAmount: CGFloat = 0
         
         func path(in rect: CGRect) -> Path {
             let rotationAdjustment = Angle.degrees(90)
@@ -35,9 +37,15 @@ struct ContentView: View {
             
             var path = Path()
             
-            path.addArc(center: CGPoint(x: rect.midX, y: rect.midY), radius: rect.width / 2, startAngle: modifiedStart, endAngle: modifiedEnd, clockwise: !clockwise)
+            path.addArc(center: CGPoint(x: rect.midX, y: rect.midY), radius: rect.width / 2 - insetAmount, startAngle: modifiedStart, endAngle: modifiedEnd, clockwise: !clockwise)
             
             return path
+        }
+        
+        func inset(by amount: CGFloat) -> some InsettableShape {
+            var arc = self
+            arc.insetAmount += amount
+            return arc
         }
     }
     
