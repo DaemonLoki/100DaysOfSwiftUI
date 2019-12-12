f826097b69835d7337e59ad5d9b9a3255d27017f
Stefan Blos <stefan.blos@gmail.com>
2019-12-12 13:58:35 +0100

    (Day 43) created and drawn Arc Shape

--- Drawing/Drawing/ContentView.swift
+++ Drawing/Drawing/ContentView.swift
@@ -23,10 +23,32 @@ struct ContentView: View {
         }
     }
     
+    struct Arc: Shape {
+        var startAngle: Angle
+        var endAngle: Angle
+        var clockwise: Bool
+        
+        func path(in rect: CGRect) -> Path {
+            let rotationAdjustment = Angle.degrees(90)
+            let modifiedStart = startAngle - rotationAdjustment
+            let modifiedEnd = endAngle - rotationAdjustment
+            
+            var path = Path()
+            
+            path.addArc(center: CGPoint(x: rect.midX, y: rect.midY), radius: rect.width / 2, startAngle: modifiedStart, endAngle: modifiedEnd, clockwise: !clockwise)
+            
+            return path
+        }
+    }
+    
     var body: some View {
-        VStack {
+        VStack(spacing: 20) {
             Triangle()
                 .stroke(Color.red, style: StrokeStyle(lineWidth: 10, lineCap: .round, lineJoin: .round))
+                .frame(width: 200, height: 200)
+            
+            Arc(startAngle: .degrees(0), endAngle: .degrees(110), clockwise: true)
+                .stroke(Color.blue, lineWidth: 10)
                 .frame(width: 200, height: 200)
         }
             
