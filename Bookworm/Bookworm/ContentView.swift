7bea9a7923d8173fb15099229d48f023dd51ee4e
Stefan Blos <stefan.blos@gmail.com>
2020-02-11 22:03:38 +0100

    (Day 53) swiftui with core data

--- Bookworm/Bookworm/ContentView.swift
+++ Bookworm/Bookworm/ContentView.swift
@@ -9,22 +9,8 @@
 import SwiftUI
 
 struct ContentView: View {
-    @Environment(\.horizontalSizeClass) var sizeClass
-    
     var body: some View {
-        if sizeClass == .compact {
-            return AnyView(VStack {
-                Text("Active size class:")
-                Text("COMPACT")
-            }
-            .font(.largeTitle))
-        } else {
-            return AnyView(HStack {
-                Text("Active size class:")
-                Text("REGULAR")
-            }
-            .font(.largeTitle))
-        }
+        Text("Hey")
     }
 }
 
