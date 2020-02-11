7d0b2c3b45c9eb44d25014bd10d187ca93c55a2b
Stefan Blos <stefan.blos@gmail.com>
2020-02-11 21:23:06 +0100

    (Day 53) created project and used Binding

---
+++ Bookworm/Bookworm/ContentView.swift
@@ -0,0 +1,45 @@
+//
+//  ContentView.swift
+//  Bookworm
+//
+//  Created by Stefan Blos on 11.02.20.
+//  Copyright © 2020 Stefan Blos. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State private var rememberMe = false
+    
+    var body: some View {
+        VStack {
+            PushButton(title: "Remember me", isOn: $rememberMe)
+            Text(rememberMe ? "On" : "Off")
+        }
+    }
+}
+
+struct PushButton: View {
+    let title: String
+    @Binding var isOn: Bool
+    
+    var onColors = [Color.red, Color.yellow]
+    var offColors = [Color(white: 0.6), Color(white: 0.4)]
+    
+    var body: some View {
+        Button(title) {
+            self.isOn.toggle()
+        }
+        .padding()
+        .background(LinearGradient(gradient: Gradient(colors: isOn ? onColors : offColors), startPoint: .top, endPoint: .bottom))
+        .foregroundColor(.white)
+        .clipShape(Capsule())
+        .shadow(radius: isOn ? 0 : 5)
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
