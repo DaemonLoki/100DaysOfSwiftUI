6979c5f72dbb1599b842e12057c23fe555578cdf
Stefan Blos <stefan.blos@gmail.com>
2020-02-20 23:48:14 +0100

    (Day 56) make 1 star books red colored

--- Bookworm/Bookworm/ContentView.swift
+++ Bookworm/Bookworm/ContentView.swift
@@ -28,6 +28,7 @@ struct ContentView: View {
                         
                         VStack(alignment: .leading) {
                             Text(book.title ?? "Unknown Title")
+                                .foregroundColor(book.rating <= 1 ? Color.red : Color.primary)
                                 .font(.headline)
                             
                             Text(book.author ?? "Unknown Author")
