ed493d9b7f6db1cb431aa31c51915d86bd4b1baf
Stefan Blos <stefan.blos@codecamp-n.com>
2020-02-26 07:49:33 +0100

    (Day 58) NSPredicate examples

--- CoreDataProject/CoreDataProject/ContentView.swift
+++ CoreDataProject/CoreDataProject/ContentView.swift
@@ -7,30 +7,46 @@
 //
 
 import SwiftUI
+import CoreData
+
+let equalsPredicate = NSPredicate(format: "universe == 'Star Wars'")
+let genericPredicate = NSPredicate(format: "universe == %@", "Star Wars")
+let smallerPredicate = NSPredicate(format: "name < %@", "F")
+let inPredicate = NSPredicate(format: "universe IN %@", ["Aliens", "Firefly", "Star Trek"])
+let beginsWithPredicate = NSPredicate(format: "name BEGINSWITH %@", "E")
+let ignoringCaseBeginsWithPredicate = NSPredicate(format: "name BEGINSWITH[c] %@", "e")
+let negatedPredicate = NSPredicate(format: "NOT name BEGINSWITH[c] %@", "e")
+
 
 struct ContentView: View {
     
     @Environment(\.managedObjectContext) var moc
-    
-    @FetchRequest(entity: Wizard.entity(), sortDescriptors: []) var wizards: FetchedResults<Wizard>
+    @FetchRequest(entity: Ship.entity(), sortDescriptors: [], predicate: equalsPredicate) var ships: FetchedResults<Ship>
     
     var body: some View {
         VStack {
-            List(wizards, id: \.self) { wizard in
-                Text(wizard.name ?? "Unknown")
+            List(ships, id: \.self) { ship in
+                Text(ship.name ?? "Unknown ship")
             }
             
-            Button("Add") {
-                let wizard = Wizard(context: self.moc)
-                wizard.name = "Harry Potter"
-            }
-            
-            Button("Save") {
-                do {
-                    try self.moc.save()
-                } catch {
-                    print(error.localizedDescription)
-                }
+            Button("Add examples") {
+                let ship1 = Ship(context: self.moc)
+                ship1.name = "Enterprise"
+                ship1.universe = "Star Trek"
+
+                let ship2 = Ship(context: self.moc)
+                ship2.name = "Defiant"
+                ship2.universe = "Star Trek"
+
+                let ship3 = Ship(context: self.moc)
+                ship3.name = "Millennium Falcon"
+                ship3.universe = "Star Wars"
+
+                let ship4 = Ship(context: self.moc)
+                ship4.name = "Executor"
+                ship4.universe = "Star Wars"
+
+                try? self.moc.save()
             }
         }
     }
