99d85090a3473d0022a0c79fb89650bbf02e844c
Stefan Blos <stefan.blos@codecamp-n.com>
2020-04-18 23:30:07 +0200

    (Day 74) accessibility with sliders

--- AccessibilityPractice/AccessibilityPractice/ContentView.swift
+++ AccessibilityPractice/AccessibilityPractice/ContentView.swift
@@ -10,32 +10,12 @@ import SwiftUI
 
 struct ContentView: View {
     
-    let pictures = [
-        "ales-krivec-15949",
-        "galina-n-189483",
-        "kevin-horstmann-141705",
-        "nicolas-tissot-335096"
-    ]
-    
-    let labels = [
-        "Tulips",
-        "Frozen tree buds",
-        "Sunflowers",
-        "Fireworks"
-    ]
-    
-    @State private var selectedPicture = Int.random(in: 0...3)
+    @State private var estimate = 25.0
     
     var body: some View {
-        Image(pictures[selectedPicture])
-        .resizable()
-        .scaledToFit()
-        .accessibility(label: Text(labels[selectedPicture]))
-        .accessibility(addTraits: .isButton)
-            .accessibility(removeTraits: .isImage)
-        .onTapGesture {
-            self.selectedPicture = Int.random(in: 0...3)
-        }
+        Slider(value: $estimate, in: 0...50)
+            .padding()
+        .accessibility(value: Text("\(Int(estimate))"))
     }
 }
 
