20746019789754f8f00a180c2cb67ad1c44a98e6
Stefan Blos <stefan.blos@codecamp-n.com>
2020-05-09 21:49:16 +0200

    (Day 79) tab view fun

--- HotProspects/HotProspects/ContentView.swift
+++ HotProspects/HotProspects/ContentView.swift
@@ -8,41 +8,34 @@
 
 import SwiftUI
 
-class User: ObservableObject {
-    @Published var name = "Taylor Swift"
-}
-
 struct ContentView: View {
-    let user = User()
+    
+    @State private var selectedTab = 0
     
     var body: some View {
-        VStack {
-            EditView()
-            DisplayView()
+        TabView(selection: $selectedTab) {
+            Text("Tab 1")
+                .onTapGesture {
+                    self.selectedTab = 1
+            }
+                .tabItem {
+                    Image(systemName: "star")
+                    Text("One")
+                }
+            .tag(0)
+            
+            Text("Tab 2")
+                .tabItem {
+                    Image(systemName: "star.fill")
+                    Text("Two")
+                }
+        .tag(1)
         }
-        .environmentObject(user)
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
-    }
-}
-
-struct EditView: View {
-    
-    @EnvironmentObject var user: User
-    
-    var body: some View {
-        TextField("Name", text: $user.name)
-    }
-}
-
-struct DisplayView: View {
-    @EnvironmentObject var user: User
-    
-    var body: some View {
-        Text(user.name)
     }
 }
