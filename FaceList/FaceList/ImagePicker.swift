327c7b131fe0b990985ecfa4c95b76988eff7f20
Stefan Blos <stefan.blos@codecamp-n.com>
2020-05-09 21:09:48 +0200

    (Day 78) changed imagepicker source to camera

--- FaceList/FaceList/ImagePicker.swift
+++ FaceList/FaceList/ImagePicker.swift
@@ -16,6 +16,7 @@ struct ImagePicker: UIViewControllerRepresentable {
     func makeUIViewController(context: Context) -> UIImagePickerController {
         let picker = UIImagePickerController()
         picker.delegate = context.coordinator
+        picker.sourceType = .camera
         return picker
     }
     
