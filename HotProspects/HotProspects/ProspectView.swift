4a4f11ef669fac3937442a461b487e7146d42314
Stefan Blos <stefan.blos@codecamp-n.com>
2020-05-12 23:18:42 +0200

    (Day 83) added qr scanning functionality2

--- HotProspects/HotProspects/ProspectView.swift
+++ HotProspects/HotProspects/ProspectView.swift
@@ -7,10 +7,13 @@
 //
 
 import SwiftUI
+import CodeScanner
 
 struct ProspectView: View {
     
     @EnvironmentObject var prospects: Prospects
+    
+    @State private var isShowingScanner = false
     
     enum FilterType {
         case none, contacted, uncontacted
@@ -53,15 +56,33 @@ struct ProspectView: View {
                 }
             }
             .navigationBarTitle(title)
-                .navigationBarItems(trailing: Button(action: {
-                    let prospect = Prospect()
-                    prospect.name = "Paul Hudson"
-                    prospect.emailAddress = "paul@hackingwithswift.com"
-                    self.prospects.people.append(prospect)
-                }) {
-                    Image(systemName: "qrcode.viewfinder")
-                    Text("Scan")
-                })
+            .navigationBarItems(trailing: Button(action: {
+                self.isShowingScanner = true
+            }) {
+                Image(systemName: "qrcode.viewfinder")
+                Text("Scan")
+            })
+                .sheet(isPresented: $isShowingScanner) {
+                    CodeScannerView(codeTypes: [.qr], simulatedData: "Paul Hudson\npaul@hackingwithswift.com", completion: self.handleScan)
+            }
+        }
+    }
+    
+    func handleScan(result: Result<String, CodeScannerView.ScanError>) {
+        self.isShowingScanner = false
+        
+        switch result {
+        case .success(let code):
+            let details = code.components(separatedBy: "\n")
+            guard details.count == 2 else { return }
+            
+            let person = Prospect()
+            person.name = details[0]
+            person.emailAddress = details[1]
+            
+            self.prospects.people.append(person)
+        case .failure(let error):
+            print("Scanning failed! Reason: \(error.localizedDescription)")
         }
     }
 }
