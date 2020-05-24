b2a9db30850e819139e0ffb65eb706b07d8d4ee8
Stefan Blos <stefan.blos@codecamp-n.com>
2020-05-24 20:40:15 +0200

    (Day 89) end with allowsHitTesting

--- Flashzilla/Flashzilla/CardView.swift
+++ Flashzilla/Flashzilla/CardView.swift
@@ -16,6 +16,7 @@ struct CardView: View {
     
     @State private var isShowingAnswer = false
     @State private var offset = CGSize.zero
+    @State private var feedback = UINotificationFeedbackGenerator()
     
     var body: some View {
         ZStack {
@@ -54,10 +55,17 @@ struct CardView: View {
             DragGesture()
                 .onChanged { gesture in
                     self.offset = gesture.translation
+                    self.feedback.prepare()
             }
                 
             .onEnded { _ in
                 if abs(self.offset.width) > 100 {
+                    if self.offset.width > 0 {
+                        self.feedback.notificationOccurred(.success)
+                    } else {
+                        self.feedback.notificationOccurred(.error)
+                    }
+                    
                     self.removal?()
                 } else {
                     self.offset = .zero
