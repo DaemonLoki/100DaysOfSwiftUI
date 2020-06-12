d0b591a6ff2f65d450597d0cadd6dfd3e88207ab
Stefan Blos <stefan.blos@codecamp-n.com>
2020-06-12 13:22:42 +0200

    (Day 98) showing alert with optional string binding

--- SnowSeeker/SnowSeeker/Resort.swift
+++ SnowSeeker/SnowSeeker/Resort.swift
@@ -21,5 +21,9 @@ struct Resort: Codable, Identifiable {
     let runs: Int
     let facilities: [String]
     
+    var facilityTypes: [Facility] {
+        facilities.map(Facility.init)
+    }
+    
     static let example = (Bundle.main.decode("resorts.json") as [Resort])[0]
 }
--- SnowSeeker/SnowSeeker/ResortView.swift
+++ SnowSeeker/SnowSeeker/ResortView.swift
@@ -11,6 +11,8 @@ import SwiftUI
 struct ResortView: View {
     @Environment(\.horizontalSizeClass) var sizeClass
     
+    @State private var selectedFacility: Facility?
+    
     let resort: Resort
     
     var body: some View {
@@ -51,9 +53,23 @@ struct ResortView: View {
                     
                     Text(ListFormatter.localizedString(byJoining: resort.facilities))
                         .padding(.vertical)
+                    
+                    HStack {
+                        ForEach(resort.facilityTypes) { facility in
+                            facility.icon
+                                .font(.title)
+                                .onTapGesture {
+                                    self.selectedFacility = facility
+                                }
+                        }
+                    }
+                    .padding(.vertical)
                 }
                 .padding(.horizontal)
             }
+        }
+        .alert(item: $selectedFacility) { facility in
+            facility.alert
         }
         .navigationBarTitle(Text("\(resort.name), \(resort.country)"), displayMode: .inline)
     }
